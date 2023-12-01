07535f2951bc3abfb9fd9ba177873e20c3bd29b8
Péter Kovács <kovacspeter84@gmail.com>
2023-12-01 13:06:11 +0100

    Add solution for Day 1

--- src/main/java/com/github/pkovacs/aoc/y2023/Day01.java
+++ src/main/java/com/github/pkovacs/aoc/y2023/Day01.java
@@ -1,5 +1,7 @@
 package com.github.pkovacs.aoc.y2023;
 
+import java.util.List;
+
 import com.github.pkovacs.aoc.AocUtils;
 import com.github.pkovacs.util.Utils;
 
@@ -8,8 +10,33 @@ public class Day01 {
     public static void main(String[] args) {
         var lines = Utils.readLines(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + solve(lines, 1));
+        System.out.println("Part 2: " + solve(lines, 2));
+    }
+
+    private static int solve(List<String> lines, int part) {
+        return lines.stream()
+                .map(line -> part == 2 ? convertDigits(line) : line)
+                .mapToInt(Day01::getCalibrationValue)
+                .sum();
+    }
+
+    private static int getCalibrationValue(String line) {
+        var digits = Utils.charsOf(line).filter(Character::isDigit).map(c -> c - '0').toList();
+        return digits.get(0) * 10 + digits.get(digits.size() - 1);
+    }
+
+    private static String convertDigits(String line) {
+        return line
+                .replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e");
     }
 
 }
