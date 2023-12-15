117b12093fcf4da9dfc9728d16071397604bac60
Péter Kovács <kovacspeter84@gmail.com>
2023-12-15 08:17:06 +0100

    Add solution for Day 15

--- src/main/java/com/github/pkovacs/aoc/y2023/Day15.java
+++ src/main/java/com/github/pkovacs/aoc/y2023/Day15.java
@@ -1,14 +1,51 @@
 package com.github.pkovacs.aoc.y2023;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import com.github.pkovacs.aoc.AbstractDay;
 
 public class Day15 extends AbstractDay {
 
     public static void main(String[] args) {
-        var lines = readLines(getInputPath());
+        var line = readFirstLine(getInputPath());
+        var steps = Arrays.stream(line.split(",")).toList();
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + solve1(steps));
+        System.out.println("Part 2: " + solve2(steps));
+    }
+
+    private static int solve1(List<String> steps) {
+        return steps.stream().mapToInt(Day15::hash).sum();
+    }
+
+    private static int solve2(List<String> steps) {
+        var boxes = IntStream.range(0, 256).mapToObj(i -> new LinkedHashMap<String, Integer>()).toList();
+        for (var step : steps) {
+            if (step.endsWith("-")) {
+                var label = step.substring(0, step.length() - 1);
+                boxes.get(hash(label)).remove(label);
+            } else {
+                var label = step.split("=")[0];
+                int power = Integer.parseInt(step.split("=")[1]);
+                boxes.get(hash(label)).put(label, power);
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 256; i++) {
+            int j = 0;
+            for (var entry : boxes.get(i).entrySet()) {
+                ans += (i + 1) * (++j) * entry.getValue();
+            }
+        }
+        return ans;
+    }
+
+    private static int hash(String str) {
+        return str.chars().reduce(0, (a, b) -> (a + b) * 17 % 256);
     }
 
 }
--- src/test/java/com/github/pkovacs/aoc/y2023/SolutionsTest.java
+++ src/test/java/com/github/pkovacs/aoc/y2023/SolutionsTest.java
@@ -33,7 +33,7 @@ public class SolutionsTest {
                 new Arguments("Day12", Day12::main, "7541", "17485169859432"),
                 new Arguments("Day13", Day13::main, "33195", "31836"),
                 new Arguments("Day14", Day14::main, "110821", "83516"),
-                new Arguments("Day15", Day15::main, "0", "0"),
+                new Arguments("Day15", Day15::main, "494980", "247933"),
                 new Arguments("Day16", Day16::main, "0", "0"),
                 new Arguments("Day17", Day17::main, "0", "0"),
                 new Arguments("Day18", Day18::main, "0", "0"),
